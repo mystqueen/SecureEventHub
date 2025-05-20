3ed97b4884a9caad8ff3bb6e185d63de6bc6de7a
rose-tetteh <rose.tetteh@amalitech.com>
2025-05-20 07:52:10 +0000

    feat:initialize project

---
+++ src/main/java/com/example/SecureEventHub/SecureEventHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecureEventHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEventHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureEventHubApplication.class, args);
+	}
+
+}
